2192ba4f83921d6658b93977db662029ed83af99
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-26 10:55:19 -0600

    refactor: add some view stuff to make it more readable, update tests

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe "Item Show", type: :feature do
       expect(page).to have_content("Item Nemo Facere")
       
       within("#description") do
-        expect(page).to have_content("Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
+        expect(page).to have_content("Description: Sunt eum id eius magni consequuntur delectus veritatis. Quisquam laborum illo ut ab. Ducimus in est id voluptas autem.")
       end
 
       within("#unit-price") do
-        expect(page).to have_content(42.91)
+        expect(page).to have_content("Unit Price: 42.91")
       end
     end
   end
